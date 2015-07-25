0b7a126cde0e36e0daf86a50cd7a1318027f6111
cfn1954 <cfn1954@users.noreply.github.com>
2015-07-25 15:52:43 -0700

    Fixed Formatting issues

--- AL EX-1.c
+++ AL EX-1.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
 	//Check that the WAVE file is OK, and pull other parts of header
 
 
-	fread(type,sizeof(char),4,fp);														//read Header RIFF
+	fread(type,sizeof(char),4,fp);								//read Header RIFF
 	if(type[0]!='R' || type[1]!='I' || type[2]!='F' || type[3]!='F')
 	{
 		printf("Expected RIFF tag\n");
@@ -57,8 +57,8 @@ int main(int argc, char *argv[])
 	}
 	printf("Found RIFF tag\n");
 
-	fread(&size, sizeof(unsigned int),1,fp);								//Needed later, read Header
-	fread(type, sizeof(char),4,fp);												//read Header WAVE
+	fread(&size, sizeof(unsigned int),1,fp);					//Needed later, read Header
+	fread(type, sizeof(char),4,fp);								//read Header WAVE
 	if (type[0]!='W' || type[1]!='A' || type[2]!='V' || type[3]!='E')
 	{
     	printf("Expected WAVE tag\n");
@@ -66,7 +66,7 @@ int main(int argc, char *argv[])
     }
     printf("Found WAVE tag\n");
     
-	fread(type,sizeof(char),4,fp);													//read Header  fmt
+	fread(type,sizeof(char),4,fp);								//read Header  fmt
 	if (type[0]!='f' || type[1]!='m' || type[2]!='t' || type[3]!=' ')
 	{
 		printf("Expected fmt tag\n");
@@ -82,10 +82,10 @@ int main(int argc, char *argv[])
 	fread(&bytesPerSample,sizeof(short),1,fp);
 	fread(&bitsPerSample,sizeof(short),1,fp);
 
-	fread(type,sizeof(char),4,fp);													//read Header  data tag
+	fread(type,sizeof(char),4,fp);								//read Header  data tag
 	if (type[0]!='d' || type[1]!='a' || type[2]!='t' || type[3]!='a')
 	{
-		printf("Expected data tag \n");											//an error here usually mean an incompatible wav file format
+		printf("Expected data tag \n");							//an error here usually mean an incompatible wav file format
 	   exit(5);
 	}
    
@@ -97,7 +97,7 @@ int main(int argc, char *argv[])
 
 	//  This is where we store PCM data, this and the following two lines is where others went wrong
 	unsigned char* buf[dataSize];  
-	//fread(buf,sizeof(unsigned char),dataSize,fp) ;
+	//fread(buf,sizeof(unsigned char),dataSize,fp) ; moved below to give bytes read
 	//fclose(fp);
 
    //Display the info about the WAVE file
@@ -109,7 +109,7 @@ int main(int argc, char *argv[])
 	printf("Bytes Per Sample: %u \n", bytesPerSample);
 	printf("Bits Per Sample: %u \n", bitsPerSample);
 	printf("Data Size: %u \n", dataSize);
-	printf("Bytes Loaded: %lu \n\n\n", fread(buf,sizeof(unsigned char),dataSize,fp));			//  cout << fread(buf,sizeof(BYTE),dataSize,fp) << " bytes loaded\n";  
+	printf("Bytes Loaded: %lu \n\n\n", fread(buf,sizeof(unsigned char),dataSize,fp)); 
 	fclose(fp); 
 
 	ALCdevice *device;
@@ -144,41 +144,41 @@ int main(int argc, char *argv[])
 	alBufferData(buffer, format, buf, dataSize, frequency);
 
 	//Sound setting variables
-	ALfloat SourcePos[] = { 0.0, 0.0, 0.0 };                                  //Position of the source sound
-	ALfloat SourceVel[] = { 0.0, 0.0, 0.0 };                                   //Velocity of the source sound
-	ALfloat ListenerPos[] = { 0.0, 0.0, 0.0 };                                //Position of the listener
-	ALfloat ListenerVel[] = { 0.0, 0.0, 0.0 };									//Velocity of the listener
-	ALfloat ListenerOri[] = { 0.0, 0.0, -1.0,  0.0, 1.0, 0.0 }; 		//Orientation of the listener
+	ALfloat SourcePos[] = { 0.0, 0.0, 0.0 };                    //Position of the source sound
+	ALfloat SourceVel[] = { 0.0, 0.0, 0.0 };                    //Velocity of the source sound
+	ALfloat ListenerPos[] = { 0.0, 0.0, 0.0 };                  //Position of the listener
+	ALfloat ListenerVel[] = { 0.0, 0.0, 0.0 };		    		//Velocity of the listener
+	ALfloat ListenerOri[] = { 0.0, 0.0, -1.0,  0.0, 1.0, 0.0 }; //Orientation of the listener
 
-	alListenerfv(AL_POSITION,    ListenerPos);                          //Set position of the listener
-	alListenerfv(AL_VELOCITY,    ListenerVel);                           //Set velocity of the listener
-	alListenerfv(AL_ORIENTATION, ListenerOri);                      //Set orientation of the listener
+	alListenerfv(AL_POSITION,    ListenerPos);                  //Set position of the listener
+	alListenerfv(AL_VELOCITY,    ListenerVel);                  //Set velocity of the listener
+	alListenerfv(AL_ORIENTATION, ListenerOri);                  //Set orientation of the listener
 
-	alSourcei (source, AL_BUFFER,   buffer);                             //Link the buffer to the source
-	alSourcef (source, AL_PITCH,    1.0f     );                              //Set the pitch of the source
-	alSourcef (source, AL_GAIN,     1.0f     );                              //Set the gain of the source
-	alSourcefv(source, AL_POSITION, SourcePos);                  //Set the position of the source
-	alSourcefv(source, AL_VELOCITY, SourceVel);                   //Set the velocity of the source
-	alSourcei (source, AL_LOOPING,  AL_FALSE );                   //Set if source is looping sound
+	alSourcei (source, AL_BUFFER,   buffer);                    //Link the buffer to the source
+	alSourcef (source, AL_PITCH,    1.0f     );                 //Set the pitch of the source
+	alSourcef (source, AL_GAIN,     1.0f     );                 //Set the gain of the source
+	alSourcefv(source, AL_POSITION, SourcePos);                 //Set the position of the source
+	alSourcefv(source, AL_VELOCITY, SourceVel);                 //Set the velocity of the source
+	alSourcei (source, AL_LOOPING,  AL_FALSE );                 //Set if source is looping sound
 
 	//PLAY 
-	alSourcePlay(source);																//Play the sound buffer linked to the source
+	alSourcePlay(source);										//Play the sound buffer linked to the source
 	printf("Press Enter to Close\n");
-	system("read");                                                            			//Pause to let the sound play
+	system("read");                                             //Stop let the sound play
 
 	//Clean-up
 
-	alDeleteSources(1, &source);                                                //Delete the OpenAL Source
-	alDeleteBuffers(1, &buffer);                                                 	//Delete the OpenAL Buffer
-	alcMakeContextCurrent(NULL);                                           //Make no context current
-	alcDestroyContext(context);                                                 //Destroy the OpenAL Context
-	alcCloseDevice(device);                                                         //Close the OpenAL Device
+	alDeleteSources(1, &source);                                //Delete the OpenAL Source
+	alDeleteBuffers(1, &buffer);                                //Delete the OpenAL Buffer
+	alcMakeContextCurrent(NULL);                                //Make no context current
+	alcDestroyContext(context);                                 //Destroy the OpenAL Context
+	alcCloseDevice(device);                                     //Close the OpenAL Device
 
 	return EXIT_SUCCESS;                                                        
 }
 
-/* Uncomment to compile from TextWrangler
+/*Uncomment to compile from  TextWrangler
 ENDOFCODE
 ./temp
-read
+read  //this keeps the terminal window open you can see errors
 */
